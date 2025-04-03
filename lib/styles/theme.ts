55a6edd997a8f68200da7233b3a0ed31e2e61bdf
Julien Wan <julien.wan@aldes.com>
2025-04-03 17:13:46 +0200

    dev: identifying the needed theme colors

---
+++ lib/styles/theme.ts
@@ -0,0 +1,40 @@
+const themeColors = {
+    foreground: '#FF0077',
+    primary: '#FF0077',
+    primaryForeground: '#FF0077',
+    destructive: '#FF0077',
+    destructiveDarker: '#FF0077',
+    destructiveForeground: '#FF0077',
+    secondary: '#FF0077',
+    secondaryDarker: '#FF0077',
+    secondaryForeground: '#FF0077',
+    muted: '#FF0077',
+    mutedForeground: '#FF0077',
+    input: '#FF0077',
+    border: '#FF0077',
+    background: '#FF0077',
+};
+
+type colorsType = typeof themeColors;
+
+export const initColors = (colors: colorsType) => {
+    console.log('setting the colors');
+    themeColors.foreground = colors.foreground;
+    themeColors.primary = colors.primary;
+    themeColors.primaryForeground = colors.primaryForeground;
+    themeColors.destructive = colors.destructive;
+    themeColors.destructiveDarker = colors.destructiveDarker;
+    themeColors.destructiveForeground = colors.destructiveForeground;
+    themeColors.secondary = colors.secondary;
+    themeColors.secondaryDarker = colors.secondaryDarker;
+    themeColors.secondaryForeground = colors.secondaryForeground;
+    themeColors.muted = colors.muted;
+    themeColors.mutedForeground = colors.mutedForeground;
+    themeColors.input = colors.input;
+    themeColors.border = colors.border;
+    themeColors.background = colors.background;
+    Object.freeze(themeColors);
+    return themeColors;
+};
+
+export const getColors: () => colorsType = () => themeColors;
